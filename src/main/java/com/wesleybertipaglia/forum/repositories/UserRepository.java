c91fd84e77dcb7566e59b6cbb47b1399268ce63d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-16 13:58:40 -0300

    feat: add UserRepository

---
+++ src/main/java/com/wesleybertipaglia/forum/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wesleybertipaglia.forum.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wesleybertipaglia.forum.models.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
