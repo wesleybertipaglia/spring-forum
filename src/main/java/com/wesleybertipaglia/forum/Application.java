c778623113408f6cf45376ab68067185ef1dfbea
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-16 14:10:05 -0300

    style: change main app name

--- src/main/java/com/wesleybertipaglia/forum/Application.java
+++ src/main/java/com/wesleybertipaglia/forum/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ForumApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ForumApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
