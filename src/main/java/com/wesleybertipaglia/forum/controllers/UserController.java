39c673c10732470ea1d1af40b1d2cd639392c82d
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-16 16:42:40 -0300

    feat: add UserService

---
+++ src/main/java/com/wesleybertipaglia/forum/controllers/UserController.java
@@ -0,0 +1,51 @@
+package com.wesleybertipaglia.forum.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.wesleybertipaglia.forum.records.UserRequestDTO;
+import com.wesleybertipaglia.forum.records.UserResponseDTO;
+import com.wesleybertipaglia.forum.services.UserService;
+
+import java.util.UUID;
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> save(@RequestBody UserRequestDTO user) {
+        return ResponseEntity.of(userService.create(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> listBanks(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(userService.list(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> get(@PathVariable UUID id) {
+        return ResponseEntity.of(userService.get(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> update(@PathVariable UUID id, @RequestBody UserRequestDTO user) {
+        return ResponseEntity.of(userService.update(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
